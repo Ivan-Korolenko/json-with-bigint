6b7d358c49b4740f1e69e98048c708d51034881d
Ivan-Korolenko <iam@ivankorolenko.com>
2023-12-27 00:25:41 +0300

    fix: fix issue #3 "Failing to parse when there is a list of numbers in a string"

--- json-with-bigint.js
+++ json-with-bigint.js
@@ -1,6 +1,6 @@
 /* 
   Function to serialize data to JSON string
-  Converts BigInt values to custom format (strings with "n" at the end) and then converts them to proper big integers in JSON string
+  Converts BigInt values to custom format (strings with digits and "n" at the end) and then converts them to proper big integers in JSON string
 */
 export const JSONStringify = (data) => {
   const bigInts = /([\[:])?"(\d+)n"([,\}\]])/g;
@@ -14,18 +14,19 @@ export const JSONStringify = (data) => {
 
 /* 
   Function to parse JSON
-  If JSON has values presented in a lib's custom format (strings with "n" character at the end), we just parse them to BigInt values (for backward compatibility with previous versions of the lib)
+  If JSON has values presented in a lib's custom format (strings with digits and "n" character at the end), we just parse them to BigInt values (for backward compatibility with previous versions of the lib)
   If JSON has values greater than Number.MAX_SAFE_INTEGER, we convert those values to our custom format, then parse them to BigInt values.
   Other types of values are not affected and parsed as native JSON.parse() would parse them.
+
+  Big numbers are found and marked using RegEx with these conditions:
+    - Before the match there's : OR :[ OR :[anyNumberOf(anyCharacters)
+    - The match itself has more than 16 digits OR (16 digits and any digit of the number is greater than that of the Number.MAX_SAFE_INTEGER)
+    - After the match there's , OR } OR ]
 */
 export const JSONParse = (json) => {
-  /*
-    Big numbers are found and marked using Regex with this condition:
-    Number's length is bigger than 16 || Number's length is 16 and any numerical digit of the number is greater than that of the Number.MAX_SAFE_INTEGER
-  */
   const numbersBiggerThanMaxInt =
-    /([\[:])?(\d{17,}|(?:[9](?:[1-9]07199254740991|0[1-9]7199254740991|00[8-9]199254740991|007[2-9]99254740991|007199[3-9]54740991|0071992[6-9]4740991|00719925[5-9]740991|007199254[8-9]40991|0071992547[5-9]0991|00719925474[1-9]991|00719925474099[2-9])))([,\}\]])/g;
-  const serializedData = json.replace(numbersBiggerThanMaxInt, '$1"$2n"$3');
+    /(?<=:|:\[|:\[.*)(\d{17,}|(?:[9](?:[1-9]07199254740991|0[1-9]7199254740991|00[8-9]199254740991|007[2-9]99254740991|007199[3-9]54740991|0071992[6-9]4740991|00719925[5-9]740991|007199254[8-9]40991|0071992547[5-9]0991|00719925474[1-9]991|00719925474099[2-9])))(?=[,\}\]])/g;
+  const serializedData = json.replace(numbersBiggerThanMaxInt, '"$1n"');
 
   return JSON.parse(serializedData, (_, value) => {
     const isCustomFormatBigInt =
