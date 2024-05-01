88fd7517e882e7c53f0fb7f40d80d30a1b22c254
Ivan Korolenko <iam@ivankorolenko.com>
2024-05-01 05:12:14 +0300

    fix: support for negative numbers

--- json-with-bigint.js
+++ json-with-bigint.js
@@ -3,7 +3,7 @@
   Converts BigInt values to custom format (strings with digits and "n" at the end) and then converts them to proper big integers in JSON string
 */
 export const JSONStringify = (data) => {
-  const bigInts = /([\[:])?"(\d+)n"([,\}\]])/g;
+  const bigInts = /([\[:])?"(-?\d+)n"([,\}\]])/g;
   const preliminaryJSON = JSON.stringify(data, (_, value) =>
     typeof value === "bigint" ? value.toString() + "n" : value
   );
@@ -19,18 +19,18 @@ export const JSONStringify = (data) => {
   Other types of values are not affected and parsed as native JSON.parse() would parse them.
 
   Big numbers are found and marked using RegEx with these conditions:
-    - Before the match there's : OR :[ OR :[anyNumberOf(anyCharacters)
-    - The match itself has more than 16 digits OR (16 digits and any digit of the number is greater than that of the Number.MAX_SAFE_INTEGER)
-    - After the match there's , OR } OR ]
+    - Before the match there is ": OR ":[ OR ":[anyNumberOf(anyCharacters) with no \ before them
+    - The match itself has more than 16 digits OR (16 digits and any digit of the number is greater than that of the Number.MAX_SAFE_INTEGER). And it may have a - sign at the start
+    - After the match there is , OR } without " after it OR ] without " after it
 */
 export const JSONParse = (json) => {
   const numbersBiggerThanMaxInt =
-    /(?<=:|:\[|:\[.*)(\d{17,}|(?:[9](?:[1-9]07199254740991|0[1-9]7199254740991|00[8-9]199254740991|007[2-9]99254740991|007199[3-9]54740991|0071992[6-9]4740991|00719925[5-9]740991|007199254[8-9]40991|0071992547[5-9]0991|00719925474[1-9]991|00719925474099[2-9])))(?=[,\}\]])/g;
-  const serializedData = json.replace(numbersBiggerThanMaxInt, '"$1n"');
+    /(?<=[^\\]":[\[]?|[^\\]":\[.*)(-?\d{17,}|-?(?:[9](?:[1-9]07199254740991|0[1-9]7199254740991|00[8-9]199254740991|007[2-9]99254740991|007199[3-9]54740991|0071992[6-9]4740991|00719925[5-9]740991|007199254[8-9]40991|0071992547[5-9]0991|00719925474[1-9]991|00719925474099[2-9])))(?=,|\}[^"]|\][^"])/g;
+  const serializedData = json.replace(numbersBiggerThanMaxInt, `"$1n"`);
 
   return JSON.parse(serializedData, (_, value) => {
     const isCustomFormatBigInt =
-      typeof value === "string" && value.match(/^\d+n$/);
+      typeof value === "string" && Boolean(value.match(/^-?\d+n$/));
 
     if (isCustomFormatBigInt)
       return BigInt(value.substring(0, value.length - 1));
